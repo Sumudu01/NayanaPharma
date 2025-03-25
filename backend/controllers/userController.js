477dbd10e636e3ab5dab6bd4b88cf79d23de700b
it21279102 <it21279102@my.sliit.lk>
2025-03-25 14:03:41 +0530

    get user by ID function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -49,6 +49,32 @@ export const registerUser = async (req, res) => {
   }
 };
 
+// Get single user by ID
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select("-__v");
+
+    if (!user) {
+      return 
+      res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+      error: error.message,
+    });
+  }
+};
+
 // Get all users
 export const getUsers = async (req, res) => {
   try {
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, getUsers } from "../controllers/userController.js";
+import { registerUser, getUsers, getUserById} from "../controllers/userController.js";
 
 const router = express.Router();
 
@@ -8,5 +8,8 @@ router.post("/register", registerUser);
 
 // Get all users route (for testing)
 router.get("/", getUsers);
+
+// Get single user route
+router.get('/:id', getUserById);
 
 export default router;
