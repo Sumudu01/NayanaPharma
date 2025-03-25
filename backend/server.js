dbb1aa3a86fc4eff3c2dff399f2e95e7719f05fe
it21279102 <it21279102@my.sliit.lk>
2025-03-25 13:09:30 +0530

    Configuration

--- backend/server.js
+++ backend/server.js
@@ -13,5 +13,5 @@ app.use(express.json());
 connectDB();
 
 // Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
