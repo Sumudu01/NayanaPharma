2e9152fea19ff78720b7bad5ea1897c79d90ed70
it21279102 <it21279102@my.sliit.lk>
2025-03-25 12:56:11 +0530

    Initialize the project and Setup the Mongodb Connection

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+// Initialize Express
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Database Connection
+connectDB();
+
+// Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
